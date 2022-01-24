7d39b0f290acaf1f1c36043f774202154538dd6f
nexbit1 <90330589+nexbit1@users.noreply.github.com>
2022-01-24 10:16:14 +0530

    ping pong game

---
+++ ball.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.create_ball()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def create_ball(self):
+        self.color('red')
+        self.penup()
+        self.shape('circle')
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.move_speed = 0.1
+        self.goto(0,0)
+        self.bounce_x()
+
+
+
+
